9e36777edadf2a2440e50f905796eb2a35d73deb
haozhihan <haozhi.han@stu.pku.edu.cn>
2022-11-09 00:31:58 +0800

    Fix github CI bug of davidson

--- source/module_hsolver/diago_david.cpp
+++ source/module_hsolver/diago_david.cpp
@@ -39,7 +39,7 @@ DiagoDavid<FPTYPE, Device>::~DiagoDavid() {
 }
 
 template<typename FPTYPE, typename Device>
-void DiagoDavid<FPTYPE, Device>::diag_mock(hamilt::Hamilt<double>* phm_in, psi::Psi<std::complex<FPTYPE>, Device> &psi, FPTYPE *eigenvalue_in)
+void DiagoDavid<FPTYPE, Device>::diag_mock(hamilt::Hamilt<FPTYPE, Device>* phm_in, psi::Psi<std::complex<FPTYPE>, Device> &psi, FPTYPE *eigenvalue_in)
 {
     if (test_david == 1)
         ModuleBase::TITLE("DiagoDavid", "diag_mock");
@@ -293,7 +293,7 @@ void DiagoDavid<FPTYPE, Device>::diag_mock(hamilt::Hamilt<double>* phm_in, psi::
 
 
 template<typename FPTYPE, typename Device>
-void DiagoDavid<FPTYPE, Device>::cal_grad(hamilt::Hamilt<double>* phm_in,
+void DiagoDavid<FPTYPE, Device>::cal_grad(hamilt::Hamilt<FPTYPE, Device>* phm_in,
                           const int &npw,
                           const int &nbase, // current dimension of the reduced basis
                           const int &notconv,
@@ -1192,7 +1192,7 @@ void DiagoDavid<FPTYPE, Device>::planSchmitOrth(
 
 #if ((defined __CUDA) || (defined __ROCM))
 template<>
-void DiagoDavid<double, psi::DEVICE_GPU>::cal_grad(hamilt::Hamilt* phm_in,
+void DiagoDavid<double, psi::DEVICE_GPU>::cal_grad(hamilt::Hamilt<double, psi::DEVICE_GPU>* phm_in,
                           const int &npw,
                           const int &nbase, // current dimension of the reduced basis
                           const int &notconv,
@@ -1207,7 +1207,7 @@ void DiagoDavid<double, psi::DEVICE_GPU>::cal_grad(hamilt::Hamilt* phm_in,
 }
 
 template<>
-void DiagoDavid<double, psi::DEVICE_GPU>::diag_mock(hamilt::Hamilt* phm_in, psi::Psi<std::complex<double>, psi::DEVICE_GPU> &psi, double* eigenvalue_in)
+void DiagoDavid<double, psi::DEVICE_GPU>::diag_mock(hamilt::Hamilt<double, psi::DEVICE_GPU>* phm_in, psi::Psi<std::complex<double>, psi::DEVICE_GPU> &psi, double* eigenvalue_in)
 {
     return;
 }
@@ -1219,7 +1219,7 @@ void DiagoDavid<double, psi::DEVICE_GPU>::diag_mock(hamilt::Hamilt* phm_in, psi:
 
 
 template<typename FPTYPE, typename Device>
-void DiagoDavid<FPTYPE, Device>::diag(hamilt::Hamilt<double> *phm_in, psi::Psi<std::complex<FPTYPE>, Device> &psi, FPTYPE* eigenvalue_in)
+void DiagoDavid<FPTYPE, Device>::diag(hamilt::Hamilt<FPTYPE, Device> *phm_in, psi::Psi<std::complex<FPTYPE>, Device> &psi, FPTYPE* eigenvalue_in)
 {
     /// record the times of trying iterative diagonalization
     int ntry = 0;
